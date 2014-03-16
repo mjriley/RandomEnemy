78d478d64e76a6931d2c758540a4926828c50e82
Matthew Riley <mjriley2@gmail.com>
2014-03-16 15:30:55 -0700

    Cleaned up script locations, updated icon display

--- Assets/Scripts/HostilityDisplay.cs
+++ Assets/Scripts/HostilityDisplay.cs
@@ -14,31 +14,52 @@ public class HostilityDisplay : MonoBehaviour
 	public int textHeight = 20;
 	public int textWidth = 100;
 
+	public int pokemonPerRow = 4;
+
 	public Texture2D spriteSheet;
 
 	private int containerX;
 	private int containerY;
+
+	private enum Pokemon
+	{
+		Pikachu,
+		Charizard,
+		Squirtle,
+		Chespin
+	}
 	
 	private List<string> labels = new List<string> ();
+	private List<Pokemon> pokemonList = new List<Pokemon> ();
 
 	public int iconColumn = 37;
 	public int iconWidth = 43;
 	public int iconHeight = 44;
 	private Texture2D myTexture;
 
+
+
+	private Dictionary<Pokemon, Point> coords = new Dictionary<Pokemon, Point>();
+	private Dictionary<Pokemon, Texture2D> textures = new Dictionary<Pokemon, Texture2D>();
+
 	// Use this for initialization
 	void Start () {
 		containerX = Screen.width - width - border;
 		containerY = border;
 
+		coords[Pokemon.Pikachu] = new Point(171, 88);
+		coords [Pokemon.Charizard] = new Point (171, 44);
+		coords [Pokemon.Squirtle] = new Point (384, 44);
+		coords [Pokemon.Chespin] = new Point (768, 1393);
+
 		addDisplayText ("Test");
 		addDisplayText ("Next Test");
 		addDisplayText ("Final Test");
 
-		//myTexture = new Texture2D (iconWidth, iconHeight);
-
-
-		//createIcon ();
+		addIcon (Pokemon.Pikachu);
+		addIcon (Pokemon.Charizard);
+		addIcon (Pokemon.Squirtle);
+		addIcon (Pokemon.Chespin);
 	}
 	
 	// Update is called once per frame
@@ -52,17 +73,26 @@ public class HostilityDisplay : MonoBehaviour
 		labels.Add (text);
 	}
 
-	void createIcon()
+	void addIcon(Pokemon pokemon)
 	{
-		myTexture = new Texture2D (iconWidth, iconHeight);
+		pokemonList.Add (pokemon);
+
+		textures [pokemon] = createIcon (pokemon);
+	}
+
+	Texture2D createIcon(Pokemon pokemon)
+	{
+		Texture2D texture = new Texture2D (iconWidth, iconHeight);
 
 		for (int x = 0; x < iconWidth; ++x) {
 			for (int y = 0; y < iconHeight; ++y)
 			{
-				myTexture.SetPixel(x, y, spriteSheet.GetPixel(171 + x, 1611 - 88 + y));
+				texture.SetPixel(x, y, spriteSheet.GetPixel(coords[pokemon].x + x, 1611 - coords[pokemon].y + y));
 			}
 		}
-		myTexture.Apply ();
+		texture.Apply ();
+
+		return texture;
 	}
 
 	void OnGUI()
@@ -74,8 +104,12 @@ public class HostilityDisplay : MonoBehaviour
 			GUI.Label (new Rect(containerX + padding, containerY + startY + i * textHeight, textWidth, textHeight), labels[i]);
 		}
 
-		createIcon ();
-		GUI.Label (new Rect (containerX + padding, containerY + startY + 100, iconWidth, iconHeight), myTexture);
+		for (int i = 0; i < pokemonList.Count; ++i)
+		{
+			int column = i % pokemonPerRow;
+			int row = i / pokemonPerRow;
+			GUI.Label(new Rect(containerX + padding + iconWidth * column, containerY + startY + 100 + iconHeight * row, iconWidth, iconHeight), textures[pokemonList[i]]);
+		}
 	}
 
 }
